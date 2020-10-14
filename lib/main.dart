f7c755ec6f1f4d63e442ecd4b87a8ae9161cc5e3
tanghongtu <tanghongtu@ut.cn>
2020-10-15 00:15:45 +0800

    image

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,16 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-            width: 500,
-            height: 400,
-            alignment: Alignment.center,
-            // color: Colors.blue,
-            child: Text("Hello Tong", style: TextStyle(fontSize: 40)),
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-            margin: EdgeInsets.all(20),
-            decoration: new BoxDecoration(
-                border: Border.all(width: 5, color: Colors.red)),
+            width: 300,
+            height: 150,
+            color: Colors.red,
+            child: new Image.network(
+              "https://i0.hdslb.com/bfs/face/b67a37542d33d093ff28780848448e0a1c78932a.png",
+              fit: BoxFit.contain,
+              repeat: ImageRepeat.noRepeat,
+              color: Colors.lightBlue,
+              colorBlendMode: BlendMode.darken,
+            ),
           ),
         ),
       ),
