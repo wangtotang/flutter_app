9738886668cb914344dee52abaf1e69f46978383
tanghongtu <tanghongtu@ut.cn>
2020-10-15 20:07:13 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(items: List.generate(1000, (i) => "item $i")));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+  MyApp({Key key, @required this.items}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 150,
-            color: Colors.red,
-            child: new Image.network(
-              "https://i0.hdslb.com/bfs/face/b67a37542d33d093ff28780848448e0a1c78932a.png",
-              fit: BoxFit.contain,
-              repeat: ImageRepeat.noRepeat,
-              color: Colors.lightBlue,
-              colorBlendMode: BlendMode.darken,
-            ),
-          ),
+        appBar: AppBar(title: Text('ListView widget')),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
         ),
       ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          width: 200,
+          color: Colors.lightBlue,
+        ),
+        Container(
+          width: 200,
+          color: Colors.lightGreen,
+        ),
+        Container(
+          width: 200,
+          color: Colors.redAccent,
+        ),
+        Container(
+          width: 200,
+          color: Colors.purpleAccent,
+        )
+      ],
     );
   }
 }
